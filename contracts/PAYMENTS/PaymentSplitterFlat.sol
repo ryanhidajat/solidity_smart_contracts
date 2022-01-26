6e955c508d4699dd0aabc527ac39c217878dc803
Ryan Hidajat <insomni4k@yahoo.com>
2022-01-27 04:38:23 +0700

    Create PaymentSplitterFlat.sol
    ready to verify

---
+++ contracts/PAYMENTS/PaymentSplitterFlat.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// Amended by HashLips
+/**
+    !Disclaimer!
+    These contracts have been used to create tutorials,
+    and was created for the purpose to teach people
+    how to create smart contracts on the blockchain.
+    please review this code on your own before using any of
+    the following code for production.
+    HashLips will not be liable in any way if for the use 
+    of the code. That being said, the code has been tested 
+    to the best of the developers' knowledge to work as intended.
+*/
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/finance/PaymentSplitter.sol";
+
+contract PAYMENTS is PaymentSplitter {
+    
+    constructor (address[] memory _payees, uint256[] memory _shares) PaymentSplitter(_payees, _shares) payable {}
+    
+}
+
+/**
+ ["0xD6F0644db0eBe04F9919bf449668E845EdE59454", 
+ "0x00C54D7E6D2F145a2De269B8873e3b38dBd9da37"]
+ */
+ 
+ /**
+ [75, 
+ 25]
+ */
